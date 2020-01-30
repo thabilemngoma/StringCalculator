23b241f40be7f2d7796675b98c0c91a29c460a2c
thabilemngoma <mkehmngoma@gmail.com>
2020-01-30 10:27:36 +0200

    update

--- src/test/java/StringCalcTest.java
+++ src/test/java/StringCalcTest.java
@@ -2,43 +2,58 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringCalcTest {
+class StringCalculatorTest {
 
-    private StringCalc stringCalc = new StringCalc();
+    private StringCalculator stringcalculator = new StringCalculator();
 
     @Test
-    void test() throws Exception{
-        assertEquals(0, stringCalc.totalS(""));assertEquals(1, stringCalc.totalS("1"));
-        assertEquals(2, stringCalc.totalS("1,1"));assertEquals(10, stringCalc.totalS("1,2,3,4"));
-        assertEquals(10, stringCalc.totalS("1,2,3,4"));assertEquals(6, stringCalc.totalS("1\n2,3"));
-        assertEquals(3, stringCalc.totalS("//;\n1;2"));assertEquals(3, stringCalc.totalS("//4\n142"));
-        assertEquals(3, stringCalc.totalS("//;\n1000;1;2"));assertEquals(6, stringCalc.totalS("//***\n1***2***3"));
-        assertEquals(6, stringCalc.totalS("//[:D][%]\n1:D2%3"));assertEquals(6, stringCalc.totalS("//[***][%%%]\n1***2%%%3"));
-        assertEquals(0, stringCalc.totalS("//[(-_-')][%]\n1(-_-')2%3"));assertEquals(7, stringCalc.totalS("//[abc][777][:(]\n1abc27773:(1"));
+    void testCaseForStringCalculator() throws Exception{
+
+
+        assertEquals(0, stringcalculator.Sum(""));
+        assertEquals(1, stringcalculator.Sum("1"));
+        assertEquals(2, stringcalculator.Sum("1,1"));
+        assertEquals(10, stringcalculator.Sum("1,2,3,4"));
+        assertEquals(10, stringcalculator.Sum("1,2,3,4"));
+        assertEquals(6, stringcalculator.Sum("1\n2,3"));
+        assertEquals(3, stringcalculator.Sum("//;\n1;2"));
+        assertEquals(3, stringcalculator.Sum("//4\n142"));
+        assertEquals(3, stringcalculator.Sum("//;\n1000;1;2"));
+        assertEquals(6, stringcalculator.Sum("//***\n1***2***3"));
+        assertEquals(6, stringcalculator.Sum("//[:D][%]\n1:D2%3"));
+        assertEquals(6, stringcalculator.Sum("//[***][%%%]\n1***2%%%3"));
+        assertEquals(0, stringcalculator.Sum("//[(-_-')][%]\n1(-_-')2%3"));
+        assertEquals(7, stringcalculator.Sum("//[abc][777][:(]\n1abc27773:(1"));
+
     }
     @Test
-    void neg() {
+    void negativeNumbersTest() {
         Exception exception = assertThrows(Exception.class, () -> {
-            stringCalc.totalS("-1,-2,3,4"); });
-        String Message = exception.getMessage();
-        assertFalse(Message.contains("Error : Negatives not allowed"));
+            stringcalculator.Sum("-1,-2,3,4");
+        });
+        String actualMessage = exception.getMessage();
+        assertFalse(actualMessage.contains("Error : Negative values are not allowed"));
+    }
+
+
+    @Test
+    void WrongDigit() throws Exception {
+        StringCalculator.Sum("//;\n1000;1;2;");
     }
     @Test
-    void negVal() throws Exception {
-        StringCalc.totalS("//;\n1000;1;2;");
-    }
-    @Test
-    void negVal1() {
+    void WrongDigitFirst() {
         Exception exception = assertThrows(Exception.class, () -> {
-            StringCalc.totalS("   //;\n1000,1;2"); });
-        String Message = exception.getMessage();
-        assertFalse(Message.contains("Invalid Input"));
+            StringCalculator.Sum("   //;\n1000,1;2");
+        });
+        String actualMessage = exception.getMessage();
+        assertFalse(actualMessage.contains("Wrong Digit"));
     }
     @Test
-    void negVal2() {
+    void WrongDigitSecond() {
         Exception exception = assertThrows(Exception.class, () -> {
-            stringCalc.totalS("1,2,3//;\n1000,1;2"); });
-        String Message = exception.getMessage();
-        assertFalse(Message.contains("the value is invalid"));
+            stringcalculator.Sum("1,2,3//;\n1000,1;2");
+        });
+        String actualMessage = exception.getMessage();
+        assertFalse(actualMessage.contains("Wrong Digit"));
     }
 }
